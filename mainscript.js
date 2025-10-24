21ad84eb72d95f1918f8682726a5e4131a37d595
Zig898 <115973478+Zig898@users.noreply.github.com>
2025-10-24 19:31:43 +0700

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,6 +1,6 @@
 function updateGMTTime() {
       const now = new Date();
-      const gmtString = now.getMilliseconds();  // gives a string like: "Tue, 25 Jul 2023 17:21:46 GMT"
+      const gmtString = now.now();  // gives a string like: "Tue, 25 Jul 2023 17:21:46 GMT"
       document.getElementById('time').textContent = gmtString;
     }
 // Update immediately and then every second
